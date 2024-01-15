79537336f5fc722d8ba377a922920ba8fc07376f
Jack Park <jackpark@gmail.com>
2024-01-15 14:15:47 -0800

    Started updatng sql for paradedb

--- sql/tagomizer.sql
+++ sql/tagomizer.sql
@@ -10,32 +10,39 @@ BEGIN
    END IF;
 END
 $do$;
-
+CREATE SEQUENCE IF NOT EXISTS public.doc_id_seq
+    AS bigint
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
 --
 -- Create a schema to hide the proxy tables from public view.
 --
-CREATE SCHEMA IF NOT EXISTS tq_tagomizer;
-GRANT ALL ON schema tq_tagomizer TO tq_proxy;
-GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA tq_tagomizer TO tq_proxy;
-ALTER DEFAULT PRIVILEGES IN SCHEMA tq_tagomizer
+CREATE SCHEMA IF NOT EXISTS public;
+GRANT ALL ON schema public TO tq_proxy;
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO tq_proxy;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public
     GRANT USAGE, SELECT ON SEQUENCES TO tq_proxy;
 
-create table if not exists tq_tagomizer.group  (
+create table if not exists public.group  (
    id text UNIQUE,
    name text not null,
    PRIMARY KEY (id)
 );
 
-create index if not exists group_idx on tq_tagomizer.group (id);
-GRANT ALL PRIVILEGES ON tq_tagomizer.group TO tq_proxy;
+create index if not exists group_idx on public.group (id);
+GRANT ALL PRIVILEGES ON public.group TO tq_proxy;
 
-create table if not exists tq_tagomizer.user  (
+create table if not exists public.user  (
    id text UNIQUE,
    PRIMARY KEY (id)
 );
 
-create index if not exists user_idx on tq_tagomizer.user (id);
-GRANT ALL PRIVILEGES ON tq_tagomizer.user TO tq_proxy;
+create index if not exists user_idx on public.user (id);
+GRANT ALL PRIVILEGES ON public.user TO tq_proxy;
+
 
 --
 --	A document exists for each Hypothes.is URL within the context of a Group, and
@@ -45,7 +52,8 @@ GRANT ALL PRIVILEGES ON tq_tagomizer.user TO tq_proxy;
 --	document_id is the id given in a hypothesis document
 -- 
 -- OK
-create table if not exists tq_tagomizer.document  (
+create table if not exists public.document  (
+	bm25_id BIGINT UNIQUE NOT NULL DEFAULT nextval('public.doc_id_seq'::regclass),
     document_id text UNIQUE,
    	url text not null,
    	title text not null,
@@ -53,104 +61,112 @@ create table if not exists tq_tagomizer.document  (
    	PRIMARY KEY (document_id)
 );
 
-create index if not exists document_url_idx on tq_tagomizer.document (url);
-create index if not exists document_did on tq_tagomizer.document(document_id);
-GRANT ALL PRIVILEGES ON tq_tagomizer.document TO tq_proxy;
-
+GRANT ALL PRIVILEGES ON public.document TO tq_proxy;
+CREATE INDEX if not exists title_idx 
+	ON public.document
+	USING bm25 ((document.*))
+	WITH (
+	  key_field=bm25_id,
+	  text_fields=title
+	);
 --
 --	A tag exists within the context of one or more documents.
 --	We pivot on tags.
 --
 -- ok
-create table if not exists tq_tagomizer.tag  (
+create table if not exists public.tag  (
    id text UNIQUE,
    name text not null,
    PRIMARY KEY (id)
 );
 
-create index if not exists tag_name on tq_tagomizer.tag (name);
-create index if not exists tag_idx on tq_tagomizer.tag (id);
-GRANT ALL PRIVILEGES ON tq_tagomizer.tag TO tq_proxy;
+create index if not exists tag_name on public.tag (name);
+create index if not exists tag_idx on public.tag (id);
+GRANT ALL PRIVILEGES ON public.tag TO tq_proxy;
 
 
 -- ok
-create table if not exists tq_tagomizer.doc_tag_ref  (
-   tag_id text not null REFERENCES  tq_tagomizer.tag (id),
-   document_id text not null REFERENCES  tq_tagomizer.document (document_id)
+create table if not exists public.doc_tag_ref  (
+   tag_id text not null REFERENCES  public.tag (id),
+   document_id text not null REFERENCES  public.document (document_id)
 );
 
-GRANT ALL PRIVILEGES ON tq_tagomizer.doc_tag_ref TO tq_proxy;
-create index if not exists dtr_tid on tq_tagomizer.doc_tag_ref (tag_id);
-create index if not exists dtr_did on tq_tagomizer.doc_tag_ref (document_id);
+GRANT ALL PRIVILEGES ON public.doc_tag_ref TO tq_proxy;
+create index if not exists dtr_tid on public.doc_tag_ref (tag_id);
+create index if not exists dtr_did on public.doc_tag_ref (document_id);
 
 -- ok
-create table if not exists tq_tagomizer.user_tag_ref  (
-   tag_id text not null REFERENCES  tq_tagomizer.tag (id),
-   user_id text not null REFERENCES  tq_tagomizer.user (id)
+create table if not exists public.user_tag_ref  (
+   tag_id text not null REFERENCES  public.tag (id),
+   user_id text not null REFERENCES  public.user (id)
 );
 
-GRANT ALL PRIVILEGES ON tq_tagomizer.user_tag_ref TO tq_proxy;
-create index if not exists utr_tid on tq_tagomizer.user_tag_ref (tag_id);
-create index if not exists utr_uid on tq_tagomizer.user_tag_ref (user_id);
+GRANT ALL PRIVILEGES ON public.user_tag_ref TO tq_proxy;
+create index if not exists utr_tid on public.user_tag_ref (tag_id);
+create index if not exists utr_uid on public.user_tag_ref (user_id);
 
-create table if not exists tq_tagomizer.user_doc_ref  (
-   document_id text not null REFERENCES  tq_tagomizer.document (document_id),
-   user_id text not null REFERENCES  tq_tagomizer.user (id)
+create table if not exists public.user_doc_ref  (
+   document_id text not null REFERENCES  public.document (document_id),
+   user_id text not null REFERENCES  public.user (id)
 );
 
-GRANT ALL PRIVILEGES ON tq_tagomizer.user_doc_ref TO tq_proxy;
-create index if not exists udr_did on tq_tagomizer.user_doc_ref (document_id);
-create index if not exists udr_uid on tq_tagomizer.user_doc_ref (user_id);
+GRANT ALL PRIVILEGES ON public.user_doc_ref TO tq_proxy;
+create index if not exists udr_did on public.user_doc_ref (document_id);
+create index if not exists udr_uid on public.user_doc_ref (user_id);
 
-create table if not exists tq_tagomizer.group_doc_ref  (
-   group_id text not null REFERENCES  tq_tagomizer.group (id),
-   document_id text not null REFERENCES tq_tagomizer.document (document_id)
+create table if not exists public.group_doc_ref  (
+   group_id text not null REFERENCES  public.group (id),
+   document_id text not null REFERENCES public.document (document_id)
 );
 
-GRANT ALL PRIVILEGES ON tq_tagomizer.group_doc_ref TO tq_proxy;
-create index if not exists gtr_did on tq_tagomizer.group_doc_ref (document_id);
-create index if not exists gtr_uid on tq_tagomizer.group_doc_ref (group_id);
+GRANT ALL PRIVILEGES ON public.group_doc_ref TO tq_proxy;
+create index if not exists gtr_did on public.group_doc_ref (document_id);
+create index if not exists gtr_uid on public.group_doc_ref (group_id);
 
-create table if not exists tq_tagomizer.group_user_ref  (
-   group_id text not null REFERENCES  tq_tagomizer.group (id),
-   user_id text not null REFERENCES tq_tagomizer.user (id)
+create table if not exists public.group_user_ref  (
+   group_id text not null REFERENCES  public.group (id),
+   user_id text not null REFERENCES public.user (id)
 );
 
-GRANT ALL PRIVILEGES ON tq_tagomizer.group_user_ref TO tq_proxy;
-create index if not exists gur_did on tq_tagomizer.group_user_ref (user_id);
-create index if not exists gur_uid on tq_tagomizer.group_user_ref (group_id);
+GRANT ALL PRIVILEGES ON public.group_user_ref TO tq_proxy;
+create index if not exists gur_did on public.group_user_ref (user_id);
+create index if not exists gur_uid on public.group_user_ref (group_id);
 
 
-create table if not exists tq_tagomizer.group_tag_ref  (
-   tag_id text not null REFERENCES  tq_tagomizer.tag (id),
-   group_id text not null REFERENCES  tq_tagomizer.group (id)
+create table if not exists public.group_tag_ref  (
+   tag_id text not null REFERENCES  public.tag (id),
+   group_id text not null REFERENCES  public.group (id)
 );
 
-GRANT ALL PRIVILEGES ON tq_tagomizer.group_tag_ref TO tq_proxy;
-create index if not exists gttr_tid on tq_tagomizer.group_tag_ref (tag_id);
-create index if not exists gttr_uid on tq_tagomizer.group_tag_ref (group_id);
+GRANT ALL PRIVILEGES ON public.group_tag_ref TO tq_proxy;
+create index if not exists gttr_tid on public.group_tag_ref (tag_id);
+create index if not exists gttr_uid on public.group_tag_ref (group_id);
+
 
 --
 --	Annotations are text objects, abstracts. They go with URLs - specific documents.
 --	We do not pivot on Annotations; they are the content of a URL (resource) view
 --  Both text and annnotations go into this table
 --
-create table if not exists tq_tagomizer.annotations  (
-   document_id text not null REFERENCES  tq_tagomizer.document (document_id),
+create table if not exists public.annotations  (
+   bm25_id BIGINT not null REFERENCES  public.document (bm25_id),
    text text not null,
    language varchar (3)
 );
 
-GRANT ALL PRIVILEGES ON tq_tagomizer.annotations TO tq_proxy;
--- CREATE INDEX IF NOT EXISTS tsv_annot_en_idx ON tq_tagomizer.annotations USING gin(tsvecs);
-CREATE INDEX IF NOT EXISTS tsv_annot_en_idx ON tq_tagomizer.annotations USING gin(to_tsvector('english', text)) WHERE language = 'en';
-create index if not exists annotations_uid on tq_tagomizer.annotations (document_id);
-
-create table if not exists tq_tagomizer.triples  (
-    document_id text not null REFERENCES  tq_tagomizer.document (document_id),
+GRANT ALL PRIVILEGES ON public.annotations TO tq_proxy;
+CREATE INDEX if not exists search_idx
+	ON public.annotations
+	USING bm25 ((annotations.*))
+	WITH (
+	  key_field='bm25_id',
+	  text_fields='text',
+);
+create table if not exists public.triples  (
+    document_id text not null REFERENCES  public.document (document_id),
    	subject text not null,
    	predicate text not null,
    	object text not null
 );
 
-GRANT ALL PRIVILEGES ON tq_tagomizer.triples TO tq_proxy;
+GRANT ALL PRIVILEGES ON public.triples TO tq_proxy;
